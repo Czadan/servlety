74b051164ebcea66ea82a16ea49b7a64a624a973
Jakub Czadankiewicz <jakub.czadankiewicz@gmail.com>
2018-10-28 10:28:32 +0100

    initial commit

---
+++ src/main/java/zadania/HelloServlet.java
@@ -0,0 +1,18 @@
+package zadania;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class HelloServlet extends HttpServlet {
+
+    // nie wspierany wiec trzeba nadpisac
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter writer = resp.getWriter();
+        writer.print("<h1>Hello World</h1>");
+    }
+}
